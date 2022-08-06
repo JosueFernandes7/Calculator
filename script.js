680abc9c236bd682750d8b97747c69d9f517fd83
Josue Fernandes <josue7mf@hotmail.com>
2022-08-06 12:10:06 -0300

    index and style finished

---
+++ script.js
@@ -0,0 +1,9 @@
+const numbers = document.querySelectorAll("[data-number]"); // check
+const operations = document.querySelectorAll("[data-operation]"); // check
+const clear = document.querySelectorAll("[data-clear]"); // check
+const delNumber = document.querySelectorAll("[data-del]"); // check
+const result = document.querySelectorAll("[data-result]"); // check
+
+const previous = document.querySelectorAll("[data-previous]");
+const current = document.querySelectorAll("[data-current]");
+console.log(previous,current);
